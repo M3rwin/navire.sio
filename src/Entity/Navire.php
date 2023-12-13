8d3da659b4e6d0e53d3b03566c5e873843b87b3e
marco.clin <marco.clin@CP-SIO-W11-PG10.bts.sio>
2023-12-13 17:00:29 +0100

    en cours de création de l'entité Navire ecriture des assertions

---
+++ src/Entity/Navire.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NavireRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NavireRepository::class)]
+class Navire
+{
+    #[Assert\Unique(fields:['imo', 'mmsi', 'indicatif'])]
+    
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 7)]
+    #[Assert\Regex('[1-9][0-9]{6}', message: 'le numéro IMO doit être unique et composé de 7 chiffres sans commencer par 0')]
+    private ?string $imo = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\Regex('.{3,}', message: 'Le nom doit être composé d\'au moins 3 carctères')]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 9)]
+    private ?string $mmsi = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $indicatif = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $eta = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImo(): ?string
+    {
+        return $this->imo;
+    }
+
+    public function setImo(string $imo): static
+    {
+        $this->imo = $imo;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getMmsi(): ?string
+    {
+        return $this->mmsi;
+    }
+
+    public function setMmsi(string $mmsi): static
+    {
+        $this->mmsi = $mmsi;
+
+        return $this;
+    }
+
+    public function getIndicatif(): ?string
+    {
+        return $this->indicatif;
+    }
+
+    public function setIndicatif(string $indicatif): static
+    {
+        $this->indicatif = $indicatif;
+
+        return $this;
+    }
+
+    public function getEta(): ?\DateTimeInterface
+    {
+        return $this->eta;
+    }
+
+    public function setEta(?\DateTimeInterface $eta): static
+    {
+        $this->eta = $eta;
+
+        return $this;
+    }
+}
