90dd9df04b43daa8d0dc7fb29d800d647093f6c0
marco.clin <marco.clin@CP-SIO-W11-PG10.bts.sio>
2024-01-08 12:58:40 +0100

    template twig page 11

---
+++ src/Controller/PortController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\PortType;
+
+#[Route('/port', name: 'port_')]
+class PortController extends AbstractController
+{
+    #[Route('/creer', name: 'creer')]
+    public function creer(Request $request, EntityManagerInterface $manager): Response
+    {
+        $port = new Port();
+        $form = $this->createForm(PortType::class, $port);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($port);
+            $manager->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('port/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
