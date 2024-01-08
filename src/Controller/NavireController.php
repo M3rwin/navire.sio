4cc20404f785aca54b6c23253fce13927ee87b08
marco.clin <marco.clin@CP-SIO-W11-PG10.bts.sio>
2024-01-08 12:34:07 +0100

    fin de l'us2 début de l'us3

--- src/Controller/NavireController.php
+++ src/Controller/NavireController.php
@@ -5,14 +5,40 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\NavireRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\NavireType;
 
+#[Route('/navire', name :'navire_')]
 class NavireController extends AbstractController
 {
-    #[Route('/navire', name: 'app_navire')]
-    public function index(): Response
+    #[Route('/voirtous', name: 'voirtous')]
+    public function voirTous(NavireRepository $repoNavire): Response
     {
-        return $this->render('navire/index.html.twig', [
-            'controller_name' => 'NavireController',
+        $navires = $repoNavire->findAll();
+        return $this->render('navire/voirtous.html.twig', [
+            'navires' => $navires,
         ]);
     }
+    
+    #[Route('/edit/{id}', name: 'edit')]
+    public function editer(int $id, Request $request, NavireRepository $repoNavire, EntityManagerInterface $em) : Response {
+        $navire = $repoNavire->find($id);
+        
+        $form = $this->createForm(NavireType::class, $navire);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()){
+            $navire = $form->getData();
+            $em->persist($navire);
+            $em->flush();
+            return $this->redirectToRoute('home');
+        }
+        return $this->render('navire/edit.html.twig', [
+            'form'=>$form->createView(),
+            'navire'=>$navire
+        ]);
+    }
+    
 }
